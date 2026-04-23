c3b53b0ac59359dbed37b08094560982d59f2443
mexhit <mexhii.asllani@gmail.com>
2026-04-23 13:24:53 +0200

    Refactor `PropertiesApi` to use `apiClient` and extend property filtering options

--- src/api/properties.js
+++ src/api/properties.js
@@ -1,25 +1,41 @@
-import axios from "axios";
+import { apiClient } from "@/api/client";
 
 class PropertiesApi {
-  getPaginatedProperties({ limit, page, fromDate, toDate, onlyUnseen }) {
-    return axios
-      .get(`http://localhost:3009/properties`, {
-        params: { limit, page, fromDate, toDate, onlyUnseen },
+  getPaginatedProperties({
+    limit,
+    page,
+    fromDate,
+    toDate,
+    onlyUnseen,
+    onlyBookmarked,
+    onlyPriceChanged,
+  }) {
+    return apiClient
+      .get("/properties", {
+        params: {
+          limit,
+          page,
+          fromDate,
+          toDate,
+          onlyUnseen,
+          onlyBookmarked,
+          onlyPriceChanged,
+        },
       })
       .then((data) => data.data);
   }
 
   getPropertiesByProviderId({ limit, page, providerId }) {
-    return axios
-      .get(`http://localhost:3009/properties/provider/${providerId}`, {
+    return apiClient
+      .get(`/properties/provider/${providerId}`, {
         params: { providerId, limit, page },
       })
       .then((data) => data.data);
   }
 
   bookmarkProperty({ propertyId, bookmarked }) {
-    return axios.put(
-      `http://localhost:3009/properties/${propertyId}/bookmark/${bookmarked}`,
+    return apiClient.put(
+      `/properties/${propertyId}/bookmark/${bookmarked}`,
     );
   }
 }
